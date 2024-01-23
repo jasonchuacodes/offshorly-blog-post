29e417ca265599102bd4cd734905f9fcaec19516
jasonchuacodes <jasonchuacodes@gmail.com>
2024-01-23 18:30:28 +0800

    [BLOG-6] Update TS types for comment input field

--- src/components/template/comment-input-field.tsx
+++ src/components/template/comment-input-field.tsx
@@ -2,11 +2,17 @@ import Button from "../base/button";
 import InputField from "../base/input-field";
 
 const CommentInputField = () => {
+    const handleChange = () => {
+        console.log('changed')
+    }
+    const handleClick = () => {
+        console.log('Submitted')
+    }
     return (
         <div className="flex flex-col space-y-1">
             <h4 className="font-bold text-sm">Comment</h4>
-            <InputField />
-            <Button label="Submit"/>
+            <InputField handleChange={handleChange} name="comment" />
+            <Button onClick={handleClick}  label="Submit"/>
         </div>
     )
 }
